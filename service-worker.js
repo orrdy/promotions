b8e0557ae22ece3458fb6f7b0eb2e65204584873
orrdy <chanaplus@gmail.com>
2025-10-07 22:16:23 +0700

    sdfcv

--- service-worker.js
+++ service-worker.js
@@ -2,10 +2,10 @@ self.addEventListener('install', event => {
   event.waitUntil(
     caches.open('my-cache01').then(cache => {
       return cache.addAll([
-        '/index.html',
-        '/icon.png',
-        '/snapshot-mobile.png',
-        '/snapshot-desktop.png'
+        'promotions/index.html',
+        'promotions/icon.png',
+        'promotions/snapshot-mobile.png',
+        'promotions/snapshot-desktop.png'
       ]);
     })
   );
